75ce0d4a977d3710dcb6360880945773cd282f96
Areeb Sajjad <areebsajjad21@gmail.com>
2022-02-27 20:35:53 +0500

    api data added

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 // useful links
 // https://api.datamuse.com/words?ml=cloud 
 // https://www.datamuse.com/api/
+
+let dataMuseURL = "https://api.datamuse.com/words?ml=";
+let dataMuseQuery = "cloud";
+dataMuseURL += `${dataMuseQuery}`;
+console.log(dataMuseURL);
+
+let giphyAPIKey = "oaf58QuzRBGlYW0E5A4W4mMJlYTQuzUW";
+let giphyURL = "api.giphy.com/v1/gifs/search";
+let giphySearchQuery = "cat";
+let giphySearchLimit = 10;
+giphyURL += `?q=${giphySearchQuery}&api_key=${giphyAPIKey}&limit=${giphySearchLimit}`;
+console.log(giphyURL);
+
